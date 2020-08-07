1940d94f0050552add147e49befc9639885fd991
wpredoy <rah415704@gmail.com>
2020-08-07 23:23:42 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// Falsy
+// false
+// 0
+// undefined
+// null
+// NaN
+// ""
+
+// Truthy
+// "0"
+// " "
+// []
+// {}
+
+let name = 0;
+
+if(name || name == 0) {
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
