7ee5d334fd403c5f354b2e7f34441898a0f260cf
wpredoy <rah415704@gmail.com>
2020-08-07 23:54:43 +0600

    nul vs undefined is clear

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+let pakhi;
+console.log(pakhi)
+
+function add(num1, num2) {
+    console.log(num1 + num2)
+    return
+}
+
+const result = add(20,25)
+console.log(result)
+
+
+function add(num1, num2) {
+    console.log(num2)
+}
+const result = add(25);
+console.log(result)
+
+const premik = {name: "Smart star", phone: 123456}
+console.log(premik.gf);
+
+const fun = undefined;
+console.log(fun)
+
+let friends = [1,3,5,10,12];
+console.log(friends[12])
